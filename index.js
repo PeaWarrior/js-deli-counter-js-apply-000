ea3c919703db95b1e65e83b2d419a7da0efbcfa6
Jackson Chen <jacksonchen171@gmail.com>
2020-03-07 14:42:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 function takeANumber(line, joined){
   line.push(joined)
-  let string = "Welcome, " + joined + ". You are number " + line.length + " in line."
-  return string
+  return "Welcome, " + joined + ". You are number " + line.length + " in line."
 }
