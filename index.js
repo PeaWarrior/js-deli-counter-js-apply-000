25b9f048b8e4f90ace3d4125ef8b15db043bcd3a
Jackson Chen <jacksonchen171@gmail.com>
2020-03-07 14:52:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,3 +14,16 @@ function nowServing(katzDeliLine){
     return "Currently serving " + serving + "."
   }
 }
+
+function currentLine(line){
+  if (line.length === 0){
+    return "The line is currently empty."
+  }
+  else {
+    let arr = []
+    for (let i = 0; i < line.length; i++){
+      arr.push(i+1 + ". " + line[i]) 
+    }
+    return "The line is current: " + arr.join(", ")
+  }
+}
