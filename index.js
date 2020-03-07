f1f2ddb5aa66f04918e28c44ea5f180e874ad94d
Jackson Chen <jacksonchen171@gmail.com>
2020-03-07 14:46:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ function takeANumber(line, joined){
   return "Welcome, " + joined + ". You are number " + line.length + " in line."
 }
 
+
 function nowServing(katzDeliLine){
   if (katzDeliLine.length === 0){
     return "There is nobody waiting to be served!"
   }
-  else {
-    return "Currently serving " + katzDeliLine[0] + "!"
-  }
+  // else {
+  //   return "Currently serving " + katzDeliLine[0] + "!"
+  // }
 }
